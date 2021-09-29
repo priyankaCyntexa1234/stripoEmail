03c4c98eb26189187b9ff7d0c998d1bcf430c21d
priyankameratwal@cyntexa.com <87518652+priyankaCyntexa1234@users.noreply.github.com>
2021-09-29 16:29:35 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 var request = require('request');
+var list1=[];
+var token, token1;
 /**
  * Render Config
  * @param req
@@ -21,36 +23,44 @@ exports.config = (req, res) => {
  * @param res
  */
 
-exports.ui = (req, res) => {
+ function stripoemails(){
+  var options = {
+    'method': 'GET',
+    'url': 'https://stripo.email/emailgeneration/v1/emails',
+    'headers': {
+      'Stripo-Api-Auth': 'eyJhbGciOiJIUzI1NiJ9.eyJzZWN1cml0eUNvbnRleHQiOiJ7XCJhcGlLZXlcIjpcIjRjZGEyNGE2LTFiMTEtNDU5My04ZjJlLTMwOWY5NjY0YmE3ZlwiLFwicHJvamVjdElkXCI6NTEwNzExfSJ9.mGEaLIpIxXeAA8v3tJv-d5S_RycJGCprjgMh31Hm9l4'
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log("in the op")
+    console.log(response.body);
+    var body2 = JSON.parse(response.body);
+    token =  body2["data"];
+    
+    for(var key in token){
+      var n=token[key].name;
+      var id=token[key].emailId;
+      console.log("name"+n);
+      console.log("name"+id);
+      list1.push({
+        'Name':token[key].name,
+        'Id':token[key].emailId
+      })
+    }
+    console.log("list1==="+JSON.stringify(list1));
+    console.log("list1==="+JSON.stringify(list1).length);
+    //console.log(response.body.data);
+  });
+
+}
+
+
+exports.ui = async(req, res) => {
  console.log("hiiiiiii")
-var options = {
-  'method': 'GET',
-  'url': 'https://stripo.email/emailgeneration/v1/emails',
-  'headers': {
-    'Stripo-Api-Auth': 'eyJhbGciOiJIUzI1NiJ9.eyJzZWN1cml0eUNvbnRleHQiOiJ7XCJhcGlLZXlcIjpcIjRjZGEyNGE2LTFiMTEtNDU5My04ZjJlLTMwOWY5NjY0YmE3ZlwiLFwicHJvamVjdElkXCI6NTEwNzExfSJ9.mGEaLIpIxXeAA8v3tJv-d5S_RycJGCprjgMh31Hm9l4'
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log("in the op")
-  console.log(response.body);
-  var body2 = JSON.parse(response.body);
-  var token =  body2["data"];
-  var list1=[];
-  for(var key in token){
-    var n=token[key].name;
-    var id=token[key].emailId;
-    console.log("name"+n);
-    console.log("name"+id);
-    list1.push({
-      'Name':token[key].name,
-      'Id':token[key].emailId
-    })
-  }
-  console.log("list1==="+JSON.stringify(list1));
-  console.log("list1==="+JSON.stringify(list1).length);
-  //console.log(response.body.data);
-});
+
+
+await stripoemails();
 console.log("byeee")
   res.render('index', {
     title: 'Custom Activity',
